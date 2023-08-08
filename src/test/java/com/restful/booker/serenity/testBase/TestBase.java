21a57508cf4ef4a9e6a67454610af9c48affd54e
bhg9295 <bhg9295@gmail.com>
2023-08-08 19:09:15 +0100

    project done

---
+++ src/test/java/com/restful/booker/serenity/testBase/TestBase.java
@@ -0,0 +1,21 @@
+package com.restful.booker.serenity.testBase;
+
+
+import com.restful.booker.serenity.utils.PropertyReader;
+import io.restassured.RestAssured;
+import org.junit.BeforeClass;
+
+/**
+ * Created by Jay
+ */
+public class TestBase {
+    public static PropertyReader propertyReader;
+
+    @BeforeClass
+    public static void init() {
+        propertyReader = PropertyReader.getInstance();
+        RestAssured.baseURI = propertyReader.getProperty("baseUrl");
+
+    }
+
+}
